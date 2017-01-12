91308887a4154f8e3835a722cb4c6225029bad71
Balazs Ando <elnesia@gmail.com>
2017-01-12 14:56:21 +0100

    chen fixed

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -57,7 +57,7 @@ public class Player {
                 if(rank2Int == null) {
                     rank2Int = Integer.parseInt(rank2) / 2;
                 }
-                if (rank1Int + rank2Int > 8) {
+                if (rank1Int + rank2Int >= 8) {
                     return currentBuyIn - currentInActionBet;
                 }
             }
