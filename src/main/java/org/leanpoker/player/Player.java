8a4d319ccae9ab9345c9ed32223222738ba43eeb
Kincses Dani <danikincs@gmail.com>
2017-01-12 11:33:13 +0100

    trying stuff

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -18,7 +18,7 @@ public class Player {
         System.err.println("Balazs log " + game.get("minimum_raise").getAsInt());
         System.err.println("Eszter log " + game.get("community_cards").getAsJsonArray().get(0));
         System.err.println("Eszter log " + game.get("community_cards").getAsJsonArray());
-        System.err.println("HOLE CARDS" + game.get("hole_cards").toString());
+        System.err.println("HOLE CARDS" + game.get("game_id"));
         return 100;
     }
 
