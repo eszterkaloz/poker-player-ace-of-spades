223025d838e11e08754bc76605f4c0fa8ed1f8b1
Kincses Dani <danikincs@gmail.com>
2017-01-12 11:42:11 +0100

    getting player

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,8 +1,10 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import jdk.nashorn.internal.ir.IfNode;
 import jdk.nashorn.internal.parser.JSONParser;
 
 import java.util.Map;
@@ -23,7 +25,13 @@ public class Player {
         System.err.println("Balazs log " + game.get("current_buy_in").getAsInt());
         System.err.println("Balazs log " + game.get("minimum_raise").getAsInt());
         System.err.println("Eszter log " + game.get("community_cards").getAsJsonArray());
-        System.err.println("HOLE CARDS" + game.get("game_id"));
+        System.err.println("HOLE CARDS" + game.get("game_id").getAsString());
+        JsonArray players = game.get("community_cards").getAsJsonArray();
+        for(JsonElement player : players){
+            System.err.println(player);
+
+        }
+
         return 100;
     }
 
