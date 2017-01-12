45a437cfd871b46588294e982c4dd07f11094155
Kincses Dani <danikincs@gmail.com>
2017-01-12 13:28:58 +0100

    megadom acuccot

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -12,12 +12,14 @@ import java.util.Map;
 public class Player {
     private static String name = "Ace of Spades";
     private static JsonArray hole_cards;
+    private static JsonObject aceofspace;
 
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
         JsonObject game = request.getAsJsonObject();
 
+        int currentInActionBet = 0;
         int currentBuyIn = game.get("current_buy_in").getAsInt();
         int minimumRaise = game.get("minimum_raise").getAsInt();
 
@@ -25,13 +27,20 @@ public class Player {
         JsonArray players = game.get("players").getAsJsonArray();
 
         for(JsonElement player : players){
+            if(player.getAsJsonObject().get("id") == game.get("in_action")) {
+                currentInActionBet = player.getAsJsonObject().get("bet").getAsInt();
+            }
             if(player.getAsJsonObject().get("name").getAsString().equals("Ace Of Spades")) {
+                aceofspace = player.getAsJsonObject();
                 hole_cards = player.getAsJsonObject().get("hole_cards").getAsJsonArray();
                 return 50;
             }
         }
-
-        return 50;
+        if (currentBuyIn == aceofspace.get("stack").getAsInt()) {
+            return 0;
+        }else {
+            return currentBuyIn - currentInActionBet;
+        }
     }
 
     public static void showdown(JsonElement game) {
