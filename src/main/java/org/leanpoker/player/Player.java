803be5f216cfbcabb59001708591ae83420d6904
eszterkaloz <eszterkaloz@gmail.com>
2017-01-12 10:47:05 +0100

    Logging game

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -18,5 +18,6 @@ public class Player {
 
     public static void showdown(JsonElement game) {
         System.err.println(game.getAsJsonObject().get("players"));
+        System.err.println(game.toString());
     }
 }
